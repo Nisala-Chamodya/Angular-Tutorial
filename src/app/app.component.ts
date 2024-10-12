2934c6ca071771d2776b341419a70eaa521b2b1d
Nisala - Chamodya <113490575+Nisala-Chamodya@users.noreply.github.com>
2024-10-12 23:04:23 +0530

    Using Template property

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { MainComponent } from "../customcomponent/main/main.component";
 import { NavigationComponent } from "./navigation/navigation.component";
 import { ContentComponent } from "./content/content.component";
+import { TemplateTestComponent } from "./template-test/template-test.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MainComponent, NavigationComponent, ContentComponent],
+  imports: [RouterOutlet, MainComponent, NavigationComponent, ContentComponent, TemplateTestComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/template-test/template-test.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template-test',
+  standalone: true,
+  imports: [],
+  template: '<h1>Templte Property </h1>',
+  styleUrl: './template-test.component.css'
+})
+export class TemplateTestComponent {
+
+}
