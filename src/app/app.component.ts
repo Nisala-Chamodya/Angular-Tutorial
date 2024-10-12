bbb76529cefcf2caaf1cbd0075154a75403a19b1
Nisala - Chamodya <113490575+Nisala-Chamodya@users.noreply.github.com>
2024-10-12 22:09:57 +0530

    component auto genaration with ng generate component <component name>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainComponent } from "../customcomponent/main/main.component";
+import { NavigationComponent } from "./navigation/navigation.component";
+import { ContentComponent } from "./content/content.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MainComponent],
+  imports: [RouterOutlet, MainComponent, NavigationComponent, ContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
